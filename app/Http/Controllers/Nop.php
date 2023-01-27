1b8597806ae4ec716078bd2977cc0e55d4a9fee6
hery <heri.azhari@idn.ccb.com>
2023-01-27 10:31:38 +0700

    base currency closing rate id correction

--- app/Http/Controllers/Nop.php
+++ app/Http/Controllers/Nop.php
@@ -9,7 +9,7 @@ use App\Market;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Session;
+use Illuminate\Validation\Rule;
 
 class Nop extends Controller
 {
